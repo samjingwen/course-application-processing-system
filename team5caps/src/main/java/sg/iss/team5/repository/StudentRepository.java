621288a0164b68cc43b9ff361257dfe06f265499
User <User@172.17.71.159>
2018-12-10 17:07:14 +0800

    added sample in CourseRepository

--- team5caps/src/main/java/sg/iss/team5/repository/StudentRepository.java
+++ team5caps/src/main/java/sg/iss/team5/repository/StudentRepository.java
@@ -5,5 +5,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import sg.iss.team5.model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, String> {
-
+//
+//	List<Course> findByNameAndId(String name, Long id);
+//
+//	List<Course> findByName(String name);
+//
+//	List<Course> countByName(String name);
+//
+//	List<Course> findByNameOrderByIdDesc(String name);
+//
+//	List<Course> deleteByName(String name);
+//
+//	@Query("Select  c  From Course c where name like '%100 Steps'")
+//	List<Course> courseWith100StepsInName();
+//
+//	@Query(value = "Select  *  From Course c where name like '%100 Steps'", nativeQuery = true)
+//	List<Course> courseWith100StepsInNameUsingNativeQuery();
+//
+//	@Query(name = "query_get_100_Step_courses")
+//	List<Course> courseWith100StepsInNameUsingNamedQuery();
 }
