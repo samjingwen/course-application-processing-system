37fe1206b3308cbc0fec38be5f51fd5385958ebf
User <User@DESKTOP-MTVDDJD>
2018-12-10 22:59:34 +0800

    made comment changes to studentRepository

--- team5caps/src/main/java/sg/iss/team5/repository/StudentRepository.java
+++ team5caps/src/main/java/sg/iss/team5/repository/StudentRepository.java
@@ -6,11 +6,11 @@ import sg.iss.team5.model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, String> {
 //
-//	List<Course> findByNameAndId(String name, Long id);
-//
-//	List<Course> findByName(String name);
-//
-//	List<Course> countByName(String name);
+//	if custom methods are needed, add the interface method down below and use another implementation with the following convention
+	
+//	1. public class StudentRepositoryImplementation implements StudentRepository {
+//	2. add the interface to this class, this will allow polymorphism
+//	source: https://dzone.com/articles/add-custom-functionality-to-a-spring-data-reposito
 //
 //	List<Course> findByNameOrderByIdDesc(String name);
 //
