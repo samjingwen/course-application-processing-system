41fe716c91e6a6b0bef8a9f307fe8dc874b43e84
ec <kamer@172.23.207.220>
2018-12-11 10:56:22 +0800

    application properties and initializer

--- team5caps/src/main/java/sg/iss/team5/caps/ServletInitializer.java
+++ team5caps/src/main/java/sg/iss/team5/caps/ServletInitializer.java
@@ -1,13 +1,24 @@
 package sg.iss.team5.caps;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+import sg.iss.team5.caps.Team5capsApplication;
+
+public class ServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Team5capsApplication.class);
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { Team5capsApplication.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
 	}
 
 }
