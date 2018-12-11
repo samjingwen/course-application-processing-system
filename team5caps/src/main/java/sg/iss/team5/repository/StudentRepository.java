87686f9cc0fa5499f3e83c05e264eeabb40a722f
Lynn Lynn Oo <Lynn Lynn Oo@LAPTOP-UHF46CF4>
2018-12-11 10:03:32 +0800

    modelchanged

--- team5caps/src/main/java/sg/iss/team5/repository/StudentRepository.java
+++ team5caps/src/main/java/sg/iss/team5/repository/StudentRepository.java
@@ -1,12 +1,15 @@
 package sg.iss.team5.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import sg.iss.team5.model.Student;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, String> {
 //
-//	if custom methods are needed, add the interface method down below and use another implementation with the following convention
+//	if custom methods are needed, add the interface method down below and 
+	//use another implementation with the following convention
 	
 //	1. public class StudentRepositoryImplementation implements StudentRepository {
 //	2. add the interface to this class, this will allow polymorphism
