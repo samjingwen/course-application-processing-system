eac241beb64988716a8eb066f4fbd7e9abfff2ad
ec <kamer@172.23.207.220>
2018-12-12 15:24:13 +0800

    updated some stuff

--- team5caps/src/main/java/sg/iss/team5/controller/StudentEnrollCourseController.java
+++ team5caps/src/main/java/sg/iss/team5/controller/StudentEnrollCourseController.java
@@ -1,18 +1,33 @@
 package sg.iss.team5.controller;
 
 import java.text.ParseException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+
+import sg.iss.team5.model.Coursedetail;
 import sg.iss.team5.model.FormattedModule;
+import sg.iss.team5.model.Studentcourse;
 import sg.iss.team5.service.StudentService;
 
 @Controller
@@ -35,11 +50,30 @@ public class StudentEnrollCourseController {
 		return mav;
 	}
 
-	@RequestMapping(value = "/student/enroll/{sid}", method = RequestMethod.POST)
-	public ModelAndView enrollStudent()
+	@RequestMapping(value = "/modules/{sid}", method = RequestMethod.POST, params= {"Enroll"})
+	public ModelAndView enrollStudent(@RequestParam("modid") String[] modid, HttpServletRequest request, HttpSession session)
 	{
-		return null;
-	
+		String[] modids = request.getParameterValues("modid");
+		
+		/*ArrayList<Studentcourse> sc = new ArrayList<Studentcourse>();
+		for(String current: modids)
+		{
+			sc.add(new Studentcourse("Enrolled",Calendar.getInstance().getTime(),))
+		}*/
+		ModelAndView mav = new ModelAndView("redirect:/modules/S00006");
+		
+		return mav;
 	}
+	
+/*	@RequestMapping(value = "/modules/{sid}", method = RequestMethod.POST)
+	public ModelAndView addAllNotTaken(@PathVariable String sid) throws ParseException {
+
+		String[] modid = request.getParameterValues("modid")
+		ModelAndView mav = new ModelAndView("availablemods");
+
+		mav.addObject("formattedmodules", mlist);
+		return mav;
+	}*/
+
 
 }
--- team5caps/src/main/webapp/WEB-INF/views/availablemods.jsp
+++ team5caps/src/main/webapp/WEB-INF/views/availablemods.jsp
@@ -1,29 +1,43 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
 <!DOCTYPE html>
 <html>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <head>
 <meta charset="ISO-8859-1">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript" charset="utf8"
+	src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+<script>
+	$(document).ready(function() {
+		$('#AvailMods').DataTable();
+	})
+</script>
+
 <title>Available Modules</title>
 </head>
+<form:form action="${pageContext.request.contextPath}/studentenroll/modules/{sid}" method="POST">
 <body>
+
+
 	<div name="availm">
-		<table id="AvailMods" class="table table-stripeds">
+		<table id="AvailMods" class="display">
 			<caption>List of available modules:</caption>
 			<br>
-			<thread>
-			<tr>
-				<td><b>Academic Year</b></td>
-				<td><b>Module ID</b></td>
-				<td><b>Module Name</b></td>
-				<td><b>Lecturer</b></td>
-				<td><b>Day of Week</b></td>
-				<td><b>Time Slot</b></td>
-				<td><b>Venue</b></td>
-				<td><b>Enroll</b></td>
-			</tr>
-			</thread>
+			<thead>
+				<tr>
+					<td><b>Academic Year</b></td>
+					<td><b>Module ID</b></td>
+					<td><b>Module Name</b></td>
+					<td><b>Lecturer</b></td>
+					<td><b>Day of Week</b></td>
+					<td><b>Time Slot</b></td>
+					<td><b>Venue</b></td>
+					<td><b>Enroll</b></td>
+				</tr>
+			</thead>
 			<tbody>
 				<c:forEach items="${formattedmodules}" var="formattedmodules"
 					varStatus="index">
@@ -37,14 +51,13 @@
 						<td>${formattedmodules.getDay()}</td>
 						<td>${formattedmodules.getTime()}</td>
 						<td>${formattedmodules.venue}</td>
-						<td><a
-							href="${pageContext.request.contextPath}/student/enroll/${student.sid}.html">
-								<b>Enroll </b>
-						</a></td>
+						<td align="center"><input type="checkbox" name="modid" value="${formattedmodules.moduleID}"></td>
 					</tr>
 				</c:forEach>
 			</tbody>
 		</table>
 	</div>
+	<input type = "submit" name="Enroll" value="Enroll">
 </body>
+</form:form>
 </html>
