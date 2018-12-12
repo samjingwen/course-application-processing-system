d7a9d9a7ca4e047d9ba209d2201e2cb32f41bec8
gaojiaxue <gaojiaxue@outlook.com>
2018-12-12 16:58:47 +0800

    dropdownlist

--- team5caps/src/main/java/sg/iss/team5/controller/LecturerGradeCourseController.java
+++ team5caps/src/main/java/sg/iss/team5/controller/LecturerGradeCourseController.java
@@ -2,12 +2,16 @@ package sg.iss.team5.controller;
 
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpSession;
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import sg.iss.team5.model.Module;
 import sg.iss.team5.model.Studentcourse;
 import sg.iss.team5.service.LecturerService;
 
@@ -16,13 +20,23 @@ import sg.iss.team5.service.LecturerService;
 public class LecturerGradeCourseController {
 	
 	@Autowired
-	LecturerService q;
+	private LecturerService lService;
 	
 	@RequestMapping(value = "/gradebook", method = RequestMethod.GET)
 	
 	public ModelAndView listAllStudentcourse(){
-		ArrayList<Studentcourse> scList = q.findAllStudentcourse();
-		ModelAndView mav = new ModelAndView("gradebook");
+		 ModelAndView mav = new ModelAndView("gradebook");
+		 String lid="L00007";
+		 //String lid =session.getAttribute("ID").toString();
+		 ArrayList<Module>sm=new ArrayList<Module>();
+		 ArrayList<String>cList=new ArrayList<String>();
+		 sm=lService.findModuleByLecturerId(lid);
+		 for (Module module : sm) {
+			 String eachcourse=module.getCoursedetail().getCourseName();
+			 cList.add(eachcourse);
+		}
+	    mav.addObject("courselist", cList); 
+		ArrayList<Studentcourse> scList = lService.findAllStudentcourse();
 		mav.addObject("scList", scList);
 		return mav;
 	}
@@ -32,5 +46,21 @@ public class LecturerGradeCourseController {
 		return "index";
 	}
 	
+	@RequestMapping(value= "/courselist")
+	@Transactional
+	public ModelAndView ShowCourseList(HttpSession session) {
+		 ModelAndView mav = new ModelAndView("gradebook");
+		 String lid="L00003";
+		 //String lid =session.getAttribute("ID").toString();
+		 ArrayList<Module>sm=new ArrayList<Module>();
+		 ArrayList<String>cList=new ArrayList<String>();
+		 sm=lService.findModuleByLecturerId(lid);
+		 for (Module module : sm) {
+			 String eachcourse=module.getCoursedetail().getCourseName();
+			 cList.add(eachcourse);
+		}
+		 mav.addObject("courselist", cList); 
+		 return mav;
+	}
 	
 }
