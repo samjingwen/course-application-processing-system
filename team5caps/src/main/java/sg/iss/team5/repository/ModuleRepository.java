4d729abebbd60f38a10157589cd8d0c085963dfd
jwjwjw123 <e0321129@u.nus.edu>
2018-12-12 23:49:10 +0800

    fixed errors

--- team5caps/src/main/java/sg/iss/team5/repository/ModuleRepository.java
+++ team5caps/src/main/java/sg/iss/team5/repository/ModuleRepository.java
@@ -29,15 +29,11 @@ public interface ModuleRepository extends JpaRepository<Module, String> {
 	@Query(value ="select moduleid from modules where lecturerid = :lid", nativeQuery=true)
 	ArrayList<Module> findModuleIdbyLectid(@Param("lid") String lid);
 
-<<<<<<< HEAD
+
 	//@Query(value = "select * from modules m where m.courseid not in (select distinct courseid from modules m where m.moduleid in (select distinct moduleid from studentcourse where studentid = :sid and grade != 'F')) and m.academicyear = :year", nativeQuery = true)
 	//ArrayList<Module> findModuleNotEnrolled(@Param("sid") String sid, @Param("year") Date year);
 
-=======
-	/*@Query(value = "select * from modules m where m.courseid not in (select distinct courseid from modules m where m.moduleid in (select distinct moduleid from studentcourse where studentid = :sid and grade != 'F')) and m.academicyear = :year", nativeQuery = true)
-	ArrayList<Module> findModuleNotEnrolled(@Param("sid") String sid, @Param("year") Date year);
-*/
->>>>>>> branch 'master' of https://github.com/jwjwjw123/team5javaca.git
+
 	@Query(value = "select * from modules m where m.moduleid = :mid", nativeQuery=true)
 	Module findByModuleID(@Param("mid") String mid);
 	
