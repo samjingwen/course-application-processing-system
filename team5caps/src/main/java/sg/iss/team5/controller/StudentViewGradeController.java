29d61ab34b9c4f11c8dfefab9536203a2570d696
ec <kamer@172.23.207.220>
2018-12-14 09:24:33 +0800

    Student: security + minor bug fix

--- team5caps/src/main/java/sg/iss/team5/controller/StudentViewGradeController.java
+++ team5caps/src/main/java/sg/iss/team5/controller/StudentViewGradeController.java
@@ -2,12 +2,15 @@ package sg.iss.team5.controller;
 
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import sg.iss.team5.caps.SecurityConfigurations;
 import sg.iss.team5.model.Studentcourse;
 import sg.iss.team5.service.StudentService;
 
@@ -18,11 +21,20 @@ public class StudentViewGradeController {
 	@Autowired
 	StudentService studentService;
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView listGrade(String sid) {
-		ArrayList<Studentcourse> cl = studentService.findCourseByStudentId("S00006");
+	public ModelAndView listGrade(HttpSession session) {
+		if (!SecurityConfigurations.CheckStudAuth(session))
+		{return new ModelAndView("redirect:/home/login");}
+		String sid = ((UserSession) session.getAttribute("USERSESSION")).getUser().getUserID();
+		ArrayList<Studentcourse> cl = studentService.findCourseByStudentId(sid);
+		ArrayList<Studentcourse> c2 = new ArrayList<Studentcourse>();
+		for(Studentcourse current: cl)
+		{if(current.getGrade() != null)
+			c2.add(current);
+		}
+		
 		ModelAndView mav = new ModelAndView("studentViewGrade");
-		mav.addObject("courseList", cl);
-		double gpa = studentService.getGpa(cl);
+		mav.addObject("courseList", c2);
+		double gpa = studentService.getGpa(c2);
 		mav.addObject("gpa", gpa);
 		return mav;
 	}
